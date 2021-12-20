8a36041f51d44b2cf88da511007689c95daae5e1
michaelahli <michael.ahli99@gmail.com>
2021-12-20 10:36:18 +0700

    time specific feature

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/michaelahli/postgre-scheduler/cmd"
@@ -40,14 +41,58 @@ func backup() {
 	if os.Getenv("AWS_S3_BACKUP") == "true" {
 		_, err = s3session.UploadObjectbyFilePath(fmt.Sprintf("./static/database-%02d-%02d-%d.sql", day, int(month), year))
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalln(err)
 		}
 	}
 
 	log.Println("[OUTPUT]", res)
 	log.Println("[OUTPUT]", s3session.GetURI())
+}
 
-	return
+func cron(scheduler *gocron.Scheduler) error {
+	duration, err := strconv.Atoi(os.Getenv("DURATION"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	isTimeSpecified, err := strconv.ParseBool(os.Getenv("IS_TIME_SPECIFIED"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	switch isTimeSpecified {
+	case true:
+		var (
+			splitTime    []int
+			timeSpecific = os.Getenv("TIME_SPECIFIC")
+			splitTimeStr = strings.Split(timeSpecific, " ")
+		)
+
+		for _, ststr := range splitTimeStr {
+			stint, err := strconv.Atoi(ststr)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			splitTime = append(splitTime, stint)
+		}
+		t := time.Date(
+			splitTime[0],
+			time.Month(splitTime[1]),
+			splitTime[2],
+			splitTime[3],
+			splitTime[4],
+			splitTime[5],
+			splitTime[6],
+			time.UTC,
+		)
+
+		scheduler.Every(uint64(duration)).Day().From(&t).Do(backup)
+
+	default:
+		scheduler.Every(uint64(duration)).Days().Do(backup)
+	}
+
+	return nil
 }
 
 func main() {
@@ -56,11 +101,6 @@ func main() {
 
 	scheduler := gocron.NewScheduler()
 
-	duration, err := strconv.Atoi(os.Getenv("DURATION"))
-	if err != nil {
-		log.Panicln(err)
-	}
-
-	scheduler.Every(uint64(duration)).Days().Do(backup)
+	cron(scheduler)
 	<-scheduler.Start()
 }
